ad98c8759a860cc370eef789ee778b8aad911a65
aegamesi <admin.aegamesi@gmail.com>
2015-11-17 21:48:28 -0800

    Update protocol version to 4.

--- src/com/aegamesi/squeebsserver/messages/MessageInLogin.java
+++ src/com/aegamesi/squeebsserver/messages/MessageInLogin.java
@@ -7,7 +7,6 @@ import java.nio.ByteBuffer;
 public class MessageInLogin extends Message {
     public String username;
     public String password;
-    public int version;
     public int rm; // ????
 
     public MessageInLogin() {
@@ -18,7 +17,6 @@ public class MessageInLogin extends Message {
     public void write(ByteBuffer b) throws IOException {
         putString(b, username);
         putString(b, password);
-        b.put((byte) version);
         b.putShort((short) rm);
     }
 
@@ -26,7 +24,6 @@ public class MessageInLogin extends Message {
     public void read(ByteBuffer b) throws IOException {
         username = getString(b);
         password = getString(b);
-        version = b.get() & 0xff;
         rm = b.getShort();
     }
 }
